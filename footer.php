ff9a6abeda4edc003c7e67576e4d2da052e16e77
SeiichiN <billie175@gmail.com>
2018-05-04 21:10:49 +0900

    footer.php modified.

--- footer.php
+++ footer.php
@@ -4,7 +4,9 @@
             </article>
             <footer>
                 <small>&copy; 2018 <a href="http://billies-works.com/">billies-works</a></small>
-				<small><a href="editList.php">登録商品編集</a></small>
+				<?php if ($loginId === 'se-ichi') { ?>
+				    <small><a href="editList.php">登録商品編集</a></small>
+                <?php } ?>
             </footer>
         </div><!-- #wrap -->
 		<script src="js/ama.js"></script>
