17d0ca363ea13dff5d9e6c5116c633d062289215
SeiichiN <billie175@gmail.com>
2019-01-14 10:57:02 +0900

    lib/MyValidator.php modified.

--- botStart.php
+++ botStart.php
@@ -7,6 +7,8 @@ require_once('UserDB.php');
 ini_set('session.cookie_httponly', true);
 session_start();
 
+$loop = React\EventLoop\Factory::create();
+
 
 $loginId = checkLoginId();
 
@@ -15,9 +17,34 @@ $mailAddress = $mydb->getMailAddress($loginId);
 
 // echo "アマゾンの現在価格を調べます。\n";
 $myobj = new Bot();
-$msg = $myobj->checkNow($loginId, $mailAddress);
+
+
+$loop->futureTick(function() use ($myobj, $loginId, $mailAddress) {
+    file_put_contents("temp.txt", "処理の中ですのだ。");
+	$msg = $myobj->checkNow($loginId, $mailAddress);
+});
+
+
+$msg = "アマゾンの現在価格を調べます。";
+$_SESSION['msg'] = $msg;
+
+$myurl = getMyURL();
+
+ob_end_flush();
+require_once 'header.php';
+echo "<h1>さいこー</h1>";
+require_once 'footer.php';
+flush();
+
+$loop->run();
+
+$msg = "現在価格を調べ終わりました。";
+file_put_contents("file.txt", $msg);
 
 $_SESSION['msg'] = $msg;
 
 $myurl = getMyURL();
-header('Location: '.$myurl.'index.php');
+require_once 'header.php';
+echo '<a href="index.php">トップへ</a>';
+require_once 'footer.php';
+flush();
